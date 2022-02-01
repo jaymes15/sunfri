63b056a5b8a2d83105169c34382f8378455551c4
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2022-02-01 16:02:00 +0100

    chore: Update heroku_env.sh file

--- heroku_env.sh
+++ heroku_env.sh
@@ -1,8 +1,8 @@
 heroku config:set DEBUG=$DEBUG_STAGING --app $HEROKU_APP_STAGING
 heroku config:set ALLOWED_HOSTS=$ALLOWED_HOST_STAGING --app $HEROKU_APP_STAGING
-heroku config:set ALLOWED_HOSTS=$THE_ONE_API_KEY --app $HEROKU_APP_STAGING
-heroku config:set ALLOWED_HOSTS=$THE_ONE_API_URL --app $HEROKU_APP_STAGING
-heroku config:set ALLOWED_HOSTS=$SECRET_KEY_STAGING --app $HEROKU_APP_STAGING
+heroku config:set THE_ONE_API_KEY=$THE_ONE_API_KEY --app $HEROKU_APP_STAGING
+heroku config:set THE_ONE_API_URL=$THE_ONE_API_URL --app $HEROKU_APP_STAGING
+heroku config:set SECRET_KEY=$SECRET_KEY_STAGING --app $HEROKU_APP_STAGING
 
 
 heroku run python manage.py makemigrations --app $HEROKU_APP_STAGING
